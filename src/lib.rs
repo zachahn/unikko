27337fbe04d0ebf7358cb05735f9a9035ef39e55
Zach Ahn <engineering@zachahn.com>
2024-01-18 08:23:04 -0500

    Implement super basic lexer
    I feel like almost everything is contextual, so it's hard to figure out
    what the lexer can do by itself.
    
    I'm sure I'll be adding to this!

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::io;
+
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    Line(String),
+    NewLine,
+    Eof,
+}
+
+pub fn tokenize(input: &mut dyn io::BufRead) -> Result<Vec<Token>, io::Error> {
+    let mut tokens = Vec::<Token>::new();
+    let mut line = String::new();
+
+    loop {
+        match input.read_line(&mut line) {
+            Ok(num) => {
+                if num == 0 {
+                    tokens.push(Token::Eof);
+                    break;
+                }
+                match line.strip_suffix("\n") {
+                    Some(stripped) => {
+                        if stripped != "" {
+                            tokens.push(Token::Line(stripped.to_string()));
+                        }
+                        tokens.push(Token::NewLine);
+                    }
+                    None => {
+                        if line != "" {
+                            tokens.push(Token::Line(line.clone()));
+                        }
+                    }
+                };
+            }
+            Err(e) => return Err(e),
+        }
+        line.clear();
+    }
+
+    Ok(tokens)
 }
 
 #[cfg(test)]
@@ -7,8 +45,42 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn no_newline() {
+        let mut input = io::Cursor::new("hello 😁".as_bytes());
+        let tokens = tokenize(&mut input).unwrap();
+        assert_eq!(
+            tokens,
+            vec!(Token::Line("hello 😁".to_string()), Token::Eof)
+        );
+    }
+
+    #[test]
+    fn with_new_line() {
+        let mut input = io::Cursor::new("hello 😁\n".as_bytes());
+        let tokens = tokenize(&mut input).unwrap();
+        assert_eq!(
+            tokens,
+            vec!(
+                Token::Line("hello 😁".to_string()),
+                Token::NewLine,
+                Token::Eof
+            )
+        );
+    }
+
+    #[test]
+    fn implicit_paragraphs() {
+        let mut input = io::Cursor::new("hello 😁\n\nyay".as_bytes());
+        let tokens = tokenize(&mut input).unwrap();
+        assert_eq!(
+            tokens,
+            vec!(
+                Token::Line("hello 😁".to_string()),
+                Token::NewLine,
+                Token::NewLine,
+                Token::Line("yay".to_string()),
+                Token::Eof
+            )
+        );
     }
 }
